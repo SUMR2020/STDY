6f362becfa0a937c5dde4af801821e74d1639ec6
Sharjeel Ali <sharjeelali@cmail.carleton.ca>
2020-01-28 13:55:09 -0500

    Gradespage: 1. Added new element creation through button press. 2. Added scrollview.

--- stdy/lib/grades_page.dart
+++ stdy/lib/grades_page.dart
@@ -1,30 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/foundation.dart';
+import 'home_widget.dart';
 
-class GradesPage extends StatelessWidget {
+class GradesPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return GradesPageState();
+  }
+}
 
-
-
-  const GradesPage ({
-    Key key,
-    @required this.onSubmit,
-    }): super(key: key);
-
-
-  final VoidCallback onSubmit;
+class GradesPageState extends State<GradesPage> {
 
   static final TextEditingController _grade = new TextEditingController();
-  String get grade => _grade.text;
 
-  @override
-  Widget build(BuildContext context){
-    return new Column(
-      children: <Widget>[
-        new TextField(controller: _grade, decoration: new InputDecoration(hintText: "0-100%"), ),
-        ],
-    );
+  GradesPageState(){
+    _children = [
+      RaisedButton(
+        child: Text('Add new Grade'),
+        onPressed: (){
+          _onAddGrade();
+        },
+      ),
+      RaisedButton(
+        child: Text('Calculate Grade'),
+        onPressed: () {
+        },
+      ),
 
-    }
+      new TextField(controller: _grade,
+        decoration: new InputDecoration(hintText: "0-100%"),),
+
+    ];
   }
+
+  String get grade => _grade.text;
+  static List<Widget> _children;
+
+
+  void _onAddGrade(){
+    setState(() {
+      _children.add(
+          new TextField(controller: _grade,
+            decoration: new InputDecoration(hintText: "0-100%"))
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+
+        child: Column(
+          children: _children
+        ),
+
+      ),
+    );
+  }
+}
 
