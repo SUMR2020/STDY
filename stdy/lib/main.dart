aad6a330fac49163822472f72a3252fe65571780
uzairirfan <m.uzair.i@hotmail.com>
2020-02-09 19:40:10 -0500

    Added theme saving

--- stdy/lib/main.dart
+++ stdy/lib/main.dart
@@ -4,8 +4,15 @@ import 'login_page.dart';
 import 'package:provider/provider.dart';
 import 'bloc/theme.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Color stdyPink = Color(0xFFFDA3A4);
+Future<bool> _themeLoaded;
+
+Future<String> loadTheme() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString('Theme') ?? "Light";
+}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,12 +23,41 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  ThemeData loadedTheme;
+  String theme;
+  Future<String> getSavedTheme() async {
+    String theme = await loadTheme();
+    return theme;
+  }
 
+  MyApp(){
+    _themeLoaded = gotTheme();
+
+  }
+
+  Future<bool> gotTheme() async{
+    theme = await loadTheme();
+    return true;
+  }
   @override
   Widget build(BuildContext context) {
+
     return ChangeNotifierProvider<ThemeChanger>(
-      create: (_) => ThemeChanger(themeStyleData[ThemeStyle.Dark]),
-      child: new MaterialAppWithTheme(),
+      create: (_) => ThemeChanger(loadedTheme),
+      child: FutureBuilder(
+        future: _themeLoaded,
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+          if (snapshot.hasData){
+          if(theme == "Light")loadedTheme = themeStyleData[ThemeStyle.Light];
+          if(theme == "Dark")loadedTheme = themeStyleData[ThemeStyle.Dark];
+          if(theme == "OLED")loadedTheme = themeStyleData[ThemeStyle.DarkOLED];
+          return new MaterialAppWithTheme();
+          }
+          else
+            return CircularProgressIndicator();
+        }
+      )
+      //new MaterialAppWithTheme(),
     );
   }
 }
