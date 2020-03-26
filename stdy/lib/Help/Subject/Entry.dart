bddf7247493f93fc2af1254be54f770fd3b35c99
planetracer <bushrawsyed@gmail.com>
2020-03-26 16:35:35 -0400

    fake entry

--- stdy/lib/Help/Subject/Entry.dart
+++ stdy/lib/Help/Subject/Entry.dart
@@ -15,7 +15,15 @@ final List<Entry> data = <Entry>[
       Entry(
         'Viewing Schedule',
         <Entry>[
-          Entry('beep'),
+          Entry("How can you see into my eyes like open doors? Leading you down,"
+        " into my core Where I've become so numb, without a soul My spirit's "
+              "sleeping somewhere cold Until you find it there, and lead it, back,"
+              " home Wake me up insideWake me up insideCall my name and save me "
+              "from the darkBid my blood to runBefore I come undoneSave me from"
+              " the nothing I've becomeNow that I know what I'm withoutYou can't"
+              " just leave meBreathe into me and make me realBring me to life Wake "
+              "me up insideWake me up insideCall my name and save me from the dark "
+              "Bid my blood to runBefore I come undoneSave me from the nothing I've become"),
         ],
       ),
       Entry(
